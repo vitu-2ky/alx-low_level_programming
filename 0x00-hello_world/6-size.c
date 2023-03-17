488d8d914c9c9d87336d9b69c8e99254f97e4726
vitu-2ky <vitumbikokambwera@gmail.com>
2023-03-17 13:12:59 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
- * Return 0 (Success)
+ * main - Entry
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char a;
