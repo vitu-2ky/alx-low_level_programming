4dcac66b990002de175a49ee438029fed0eb256d
vitu-2ky <vitumbikokambwera@gmail.com>
2023-03-17 11:38:41 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long int: %i bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(s)\n", sizeof(float);
+	printf("Size of a float: %i byte9s)\n", sizeof(float));
 	return (0);
 }
