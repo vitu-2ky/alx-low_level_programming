7c4f18becebdd95e28c669e2f1767d81501e5fb4
vitu-2ky <vitumbikokambwera@gmail.com>
2023-03-17 13:43:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to the standard error
+ * main - A c program that prints a line to the standard error
  * Return 1 (Success)
  */
 int main(void)
