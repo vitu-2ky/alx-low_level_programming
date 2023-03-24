611807a6a3d4a2ab76e7dcd09c93af4f0cc815d0
vitu-2ky <vitumbikokambwera@gmail.com>
2023-03-24 04:11:23 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 	{
 	for (j = 1; j <= 14; j++)
 	{
-	if (j >= 10)
-	_putchar('1');
-	_putchar(j % 10 + '0');
+		if (j >= 10)
+		_putchar('1');
+		_putchar(j % 10 + '0');
 	}
 	_putchar('\n');
 	}
