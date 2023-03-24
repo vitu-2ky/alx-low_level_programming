ee93cdedb20eb7661f8e4355276709e6e2f296d2
vitu-2ky <vitumbikokambwera@gmail.com>
2023-03-24 00:10:02 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - Checks if a letter is an uppercase
- *  @c: input character
- *  Return: 1 or 0
+ * _isupper - Checks for lowercase character
+ *  @c: The character to be checked
+ *  Return: 1 for uppercase character or 0 for anything else
  */
 
 int _isupper(int c)
