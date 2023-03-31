ff0bfb1ae3ba007f677e16ee6402b5923c566074
vitu-2ky <vitumbikokambwera@gmail.com>
2023-03-31 09:28:06 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -23,7 +23,7 @@ void print_buffer(char *b, int size)
 	{
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
-		for (i = 0; i < 1110; i++)
+		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
 				printf("%02x", *(b + o + i));
