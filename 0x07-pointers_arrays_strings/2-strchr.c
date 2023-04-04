b892551564cc1d07ac9a3775d647b9590380dbea
vitu-2ky <vitumbikokambwera@gmail.com>
2023-04-04 01:35:29 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: a string
+ * @c: character occurance
+ * Return: c a pointer to first occurance and
+ * NULL if c is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return ('\0');
+}
