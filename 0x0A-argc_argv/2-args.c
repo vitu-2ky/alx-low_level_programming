a77bc186622ad93953728041a1b1a462f4592be2
vitu-2ky <vitumbikokambwera@gmail.com>
2023-04-12 00:36:07 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - writes a program that prints all arguments it recives
+ * @argc: the nunber of arguments count
+ * @argv: the array of arguments vector
+ * Return: always 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; n < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
