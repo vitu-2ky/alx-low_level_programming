463437e1ba93ed5fdb7417119ef80f8ad05f6770
vitu-2ky <vitumbikokambwera@gmail.com>
2023-04-17 12:58:04 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about a new type of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ * Description: dogs description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+#endif
