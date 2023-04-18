77cbc19ccd7824b5473724599c7d279297824294
vitu-2ky <vitumbikokambwera@gmail.com>
2023-04-18 00:01:13 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: dog to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
